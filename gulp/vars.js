95e923079611b62e006e181338b932683a0b114b
Guillaume Chambard <guillaume@marcovasco.fr>
2015-09-22 17:25:05 +0800

    first commit

---
+++ gulp/vars.js
@@ -0,0 +1,37 @@
+var src_path = './src/',
+    prod_path = './app/';
+
+module.exports = {
+  src : {
+    basedir: src_path,
+    jade : [
+      src_path+'jade/**/*.jade'
+    ],
+    assets : [
+      src_path+'assets/**/*'
+    ]
+  },
+  prod : {
+    basedir: prod_path,
+    js : prod_path+'js',
+    html : prod_path,
+    assets : prod_path+'assets'
+  },
+  watch : {
+    jade : src_path+'/jade/**/*.jade',
+    assets : src_path+'/assets/**/*'
+  },
+  onError: {
+    title: "Error <%= error.plugin %> : <%= error.name %>",
+    message: '\
+<% if (typeof(error.message) !== "undefined") { %>\
+  <%= error.message %>\
+<% } %>\
+<% if (typeof(error.filename) !== "undefined") { %>\
+  in file <%= error.filename %>\
+  <% if (typeof(error.location) !== "undefined") { %>\
+    on line <%= error.location.first_line %> and col <%= error.location.first_column %>\
+  <% } %>\
+<% } %>'
+  }
+}
